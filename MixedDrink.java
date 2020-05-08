0a7921c3808a854f0faf0266ea862071b4b4a739
DarleneAntonino <62381676+DarleneAntonino@users.noreply.github.com>
2020-05-08 10:00:11 +0200

    upload MixedDrink.java
    Just the starting point for the MixedDrink class

---
+++ MixedDrink.java
@@ -0,0 +1,9 @@
+/*
+Created: Darlene Antonino
+Last changed: 08.05.2020; Darlene Antonino
+Description: this class is a subclass from the "" class; it contains more complex drinks, which consist of more then
+one liquid
+ */
+
+public class MixedDrink {
+}
