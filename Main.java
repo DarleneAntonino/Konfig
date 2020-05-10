236451584c265710a0b3641685a7d89a738dbdcf
ViolaSchlocker <62704034+ViolaSchlocker@users.noreply.github.com>
2020-05-10 20:30:34 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -24,7 +24,7 @@ public class Main {
         Liquid gin1 = new Liquid("Beefeater London Dry, Gin", 0.09, 40);
         Liquid vermouth2 = new Liquid("Martini Fiero, Vermouth", 0.015, 14.9);
         Liquid spWater2 = new Liquid("Saskia, Sparkling Water", 1, 0);
-        Liquid hotWater = new Liquid("Voeslauer, Still", 0.5, 0);
+        Liquid water = new Liquid("Voeslauer, Still", 0.5, 0);
         Liquid coffee = new Liquid("Kaffee Partner, Bio Crema", 0.030, 0);
         Liquid milk1 = new Liquid("Stainzer, Vollmilch", 0.1, 0);
         Liquid milk2 = new Liquid("Stainzer, Vollmilch", 0.25, 0);
@@ -92,10 +92,8 @@ public class Main {
         List<Liquid> latteMacch = new ArrayList<Liquid>();
         latteMacch.add(milk2);
         latteMacch.add(coffee);
-        HotBeverage latteMachiatto = new HotBeverage("Latte Macchiatto", latteMacch);
-        
-        
-        
+        HotBeverage latteMacchiatto = new HotBeverage("Latte Macchiatto", latteMacch);
+          
         //Irish Coffee
         List<Liquid> irishC = new ArrayList<Liquid>();
         irishC.add(milk2);
